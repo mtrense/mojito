2e6b05f1edbe822886305421846ac292425c0b16
Max Trense <dev@trense.info>
2012-05-14 16:11:25 +0200

    add specs for rendering module

--- spec/mojito/rendering_spec.rb
+++ spec/mojito/rendering_spec.rb
@@ -4,8 +4,8 @@ require 'mojito'
 describe Mojito::Rendering::Content do
 	
 	subject do
-		Mojito.application Mojito::Matchers::Methods do
-			on GET() do write 'test content' ; halt! end
+		Mojito.base_application Mojito::Rendering::Content do
+			on do write 'test content' ; halt! end
 		end.mock_request
 	end
 	
