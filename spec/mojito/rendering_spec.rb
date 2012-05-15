cf70d569426615db5ed1970860015be85c72d90a
Max Trense <dev@trense.info>
2012-05-15 21:44:16 +0200

    add specs

--- spec/mojito/rendering_spec.rb
+++ spec/mojito/rendering_spec.rb
@@ -15,3 +15,19 @@ describe Mojito::Rendering::Content do
 	it { subject.get('/').body.should == 'test content' }
 	
 end
+
+describe Mojito::Rendering::StatusCodes do
+	
+	subject do
+		Mojito.base_application Mojito::Matchers::Path, Mojito::Rendering::StatusCodes do
+			on 'ok' do ok! end
+			on 'not_found' do not_found! end
+			on 'internal_server_error' do internal_server_error! end
+		end.mock_request
+	end
+	
+	it { subject.get('/ok').status.should == 200 }
+	it { subject.get('/not_found').status.should == 404 }
+	it { subject.get('/internal_server_error').status.should == 500 }
+	
+end
