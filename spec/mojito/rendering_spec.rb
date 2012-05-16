4b91553dd780d6644a2566a506913dbf4fb69ef1
Max Trense <dev@trense.info>
2012-05-16 13:12:49 +0200

    update rendering specs

--- spec/mojito/rendering_spec.rb
+++ spec/mojito/rendering_spec.rb
@@ -23,11 +23,16 @@ describe Mojito::Rendering::StatusCodes do
 			on 'ok' do ok! end
 			on 'not_found' do not_found! end
 			on 'internal_server_error' do internal_server_error! end
+			on 'unavailable' do unavailable! end
+			on 'redirect' do redirect! '/test' end
 		end.mock_request
 	end
 	
 	it { subject.get('/ok').status.should == 200 }
 	it { subject.get('/not_found').status.should == 404 }
 	it { subject.get('/internal_server_error').status.should == 500 }
+	it { subject.get('/unavailable').status.should == 503 }
+	it { subject.get('/redirect').status.should == 302 }
+	it { subject.get('/redirect').headers['Location'].should == '/test' }
 	
 end
