3d7dc6fa41ba9ac1c1cc086d5866ccc2ee9a82ca
Max Trense <dev@trense.info>
2012-05-17 21:57:59 +0200

    specs for Rack::Request-extension

---
+++ spec/mojito/request_extensions_spec.rb
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+require 'simplecov' and SimpleCov.start do
+	add_filter "spec/"
+end
+require 'mojito'
+
+describe ::Rack::Request do
+	
+	subject do
+		Mojito.base_application Mojito::Rendering::Content do
+			write request.GET[:hello]
+			halt!
+		end.mock_request
+	end
+	
+	it { subject.get('/?hello=world').status.should == 200 }
+	it { subject.get('/?hello=world').body.should == 'world' }
+	
+end
