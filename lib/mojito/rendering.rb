a7f529f6e331d01b8dbc73bdf74610f7d5171ac7
Max Trense <dev@trense.info>
2012-05-23 18:13:53 +0200

    template rendering

--- lib/mojito/rendering.rb
+++ lib/mojito/rendering.rb
@@ -7,11 +7,13 @@ module Mojito::Rendering
 	require 'mojito/rendering/delegation'
 	require 'mojito/rendering/file'
 	require 'mojito/rendering/status_codes'
+	require 'mojito/rendering/templates'
 	
 	include Content
 	include ContentTypes
 	include Delegation
 	include File
 	include StatusCodes
+	include Templates
 	
 end
--- lib/mojito/rendering/templates.rb
+++ lib/mojito/rendering/templates.rb
@@ -4,9 +4,21 @@ module Mojito::Rendering
 	
 	module Templates
 		require 'tilt'
+		require 'where'
+		require 'mime/types'
 		
-		def template(name, locals = {})
-			
+		def template(*args, &block)
+			locals = Hash === args.last ? args.pop : self.locals
+			template = if args.size == 2
+				Tilt[args.first].new { args.last }
+			elsif args.size == 1
+				file = Where.cdir(1) + args.first
+				if %r{\.(?<extension>\w+)\.\w+$} =~ file.to_s
+					response['Content-Type'] = MIME::Types.type_for(extension)
+				end
+				Tilt[file.to_s].new file.to_s
+			end
+			response.write template.render(self, locals, &block)
 		end
 		
 	end
