96c05076de9778a7ca5123d8166baff29739128e
Max Trense <dev@trense.info>
2012-05-30 00:02:16 +0200

    add render_as

--- lib/mojito/rendering/content.rb
+++ lib/mojito/rendering/content.rb
@@ -8,6 +8,15 @@ module Mojito::Rendering
 			response.write content
 		end
 		
+		def render_as(type, content, *args)
+			renderer = "to_#{type}".to_sym
+			if content.respond_to? renderer
+				response.write(content.send(renderer, *args[0...content.method(renderer).arity]))
+			else
+				Mojito::Rendering::StatusCodes.instance_method(:not_found!).bind(self).call
+			end
+		end
+		
 	end
 
 end
