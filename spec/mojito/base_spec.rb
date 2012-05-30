2d5e5362e9c22dbfb7a4d7f849590e13a291e7c1
Max Trense <dev@trense.info>
2012-05-30 19:42:11 +0200

    add specs

--- spec/mojito/base_spec.rb
+++ spec/mojito/base_spec.rb
@@ -6,6 +6,17 @@ require 'mojito'
 
 describe Mojito::Base do
 	
+	subject do
+		Mojito.base_application :status_codes do
+			on Mojito::Matchers::PATH('test.:extension') do ok! end
+		end.mock_request
+	end
 	
+	it { subject.get('/test.html').headers.should include('Content-Type') }
+	it { subject.get('/test.html').headers['Content-Type'].should == 'text/html' }
+	it { subject.get('/test.txt').headers.should include('Content-Type') }
+	it { subject.get('/test.txt').headers['Content-Type'].should == 'text/plain' }
+	it { subject.get('/test.rb').headers.should include('Content-Type') }
+	it { subject.get('/test.rb').headers['Content-Type'].should == 'application/x-ruby' }
 	
 end
