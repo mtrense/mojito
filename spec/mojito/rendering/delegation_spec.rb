8b1f0484f663a5eb0756080f1673f548a201705a
Max Trense <dev@trense.info>
2012-06-03 21:13:56 +0200

    add specs for delegation renderer

--- spec/mojito/rendering/delegation_spec.rb
+++ spec/mojito/rendering/delegation_spec.rb
@@ -7,21 +7,23 @@ require 'mojito'
 describe Mojito::Rendering::Delegation do
 	
 	subject do
-		sub_app = Mojito.base_application Mojito::R::Content, Mojito::R::Delegation do
+		sub_app = Mojito.base_application Mojito::R::Content, Mojito::M::Path do
+			on PATH('this/is/the/sub-application') do write request.context_path ; halt! end
 			on true do write 'sub-application' ; halt! end
 		end
 		Mojito.base_application Mojito::Rendering::Content, Mojito::Rendering::Delegation do
-			on true do run! sub_app end
+			on 'to/sub/app' do run! sub_app end
 		end.mock_request
 	end
 	
-	it { subject.get('/').status.should == 200 }
-	it { subject.get('/').body.should == 'sub-application' }
+	it { subject.get('/to/sub/app').status.should == 200 }
+	it { subject.get('/to/sub/app').body.should == 'sub-application' }
+	it { subject.get('/to/sub/app/this/is/the/sub-application').body.should == '/to/sub/app' }
 	
 	context do
 		
 		subject do
-			sub_app = Mojito.base_application Mojito::R::Content, Mojito::R::Delegation, Mojito::H::Shortcuts do
+			sub_app = Mojito.base_application Mojito::R::Content, Mojito::H::Shortcuts do
 				on true do write("#{path_info} #{captures.first}") ; halt! end
 			end
 			Mojito.base_application Mojito::M::Path, Mojito::R::Content, Mojito::R::Delegation do
