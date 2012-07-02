30575ee400c77870b36a3700afb291948bb85566
Max Trense <dev@trense.info>
2012-07-02 13:42:13 +0200

    check for previously set content-type when using type-guessing

--- lib/mojito/rendering/templates.rb
+++ lib/mojito/rendering/templates.rb
@@ -14,7 +14,7 @@ module Mojito::Rendering
 			elsif args.size == 1
 				file = Where.cdir(1) + args.first
 				Mojito::R::StatusCodes.instance_method(:not_found!).bind(self).call unless file.exist?
-				if %r{\.(?<extension>\w+)\.\w+$} =~ file.to_s
+				if not response.include?('Content-Type') and %r{\.(?<extension>\w+)\.\w+$} =~ file.to_s
 					response['Content-Type'] = MIME::Types.type_for(extension)
 				end
 				Tilt[file.to_s].new file.to_s
